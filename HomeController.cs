1922e6e9f50b57175853c66dbb143cddc0408414
sangeethaksundaramurthy <97410162+sangeethaksundaramurthy@users.noreply.github.com>
2022-01-10 12:09:21 +0100

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TMDB.Models;
+
+namespace TMDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        private readonly HttpClient HttpClient;
+        private readonly string apikey = "";
+        private readonly string apiurl = "";
+        private MovieTVShowResults? MovieTVShowResultsObj;
+        private List<MovieTVShowDetails> MovieTVShowObj;  
+        public HomeController(IConfiguration configuration, List<MovieTVShowDetails> movies)
+        {
+            this.apikey = configuration["api:apikey"];
+            this.apiurl = configuration["api:apiurl"];            
+            HttpClient = new HttpClient();
+            this.MovieTVShowObj = movies;
+        }
+        private async Task<MovieTVShowResults> LoadBestRatedMovies()
+        {
+            var url = this.apiurl + "/movie?certification_country=US&certification=R&sort_by=vote_average.desc&api_key=" + this.apikey;
+            return await HttpClient.GetFromJsonAsync<MovieTVShowResults>(url);
+        }
+        private async Task<MovieTVShowResults> LoadBestRatedTVShows()
+        {
+            var url = this.apiurl + "/tv?certification_country=US&certification=R&sort_by=vote_average.desc&api_key=" + this.apikey;
+            return await HttpClient.GetFromJsonAsync<MovieTVShowResults>(url);
+        }
+        public async Task<ActionResult> OnLoad()
+        {  
+            MovieTVShowResultsObj = await LoadBestRatedMovies();
+            int load = 1;
+            foreach (var item in MovieTVShowResultsObj.Results)
+            {
+                
+                if (load <=10)
+                {
+                    MovieTVShowObj.Add(item);
+                    load++;
+                }
+                
+            }
+            MovieTVShowResultsObj = await LoadBestRatedTVShows();
+            load = 1;
+            foreach (var item in MovieTVShowResultsObj.Results)
+            {                
+                if (load <= 10)
+                {
+                    MovieTVShowObj.Add(item);
+                    load++;
+                }
+            }
+            return View("DisplayMovie", MovieTVShowObj);
+        }
+
+       
+        public ActionResult DisplayMovieAction(int id)
+        {   
+            var movie = ((List<MovieTVShowDetails>)MovieTVShowObj)[id];
+            return View("DisplayMovieDetails", movie);
+        }
+
+        public ActionResult SearchButtonOnClick(string MovieName)
+        {
+            var movies =   (from temp in MovieTVShowObj
+                            where temp.Original_title.IndexOf(MovieName, StringComparison.CurrentCultureIgnoreCase)!= -1
+                            || temp.Original_name.IndexOf(MovieName, StringComparison.CurrentCultureIgnoreCase) != -1
+                            select temp).ToList<MovieTVShowDetails>(); 
+            return View("DisplayMovie", movies);
+        }
+    }
+}
---
+++ MovieTVShowDetails.cs
@@ -0,0 +1,51 @@
+﻿
+namespace TMDB.Models
+{
+    public class MovieTVShowResults
+    {
+        private List<MovieTVShowDetails>? results;
+        
+        public List<MovieTVShowDetails>? Results { get => results; set => results = value; }
+        
+    }
+    public class MovieTVShowDetails
+    {
+        private bool adult;
+        private string backdrop_path = "";
+        private int[]? genre_ids;
+        private int id;
+        private string original_language = "";
+        private string original_name = "";
+        private string original_title = "";
+        private string name = "";
+        private string overview = "";
+        private float popularity;
+        private string poster_path = "";
+        private DateTime release_date;
+        private String first_air_date = "";
+        private string title = "";
+        private bool video;
+        private float vote_average;
+        private int vote_count;
+
+        public bool Adult { get => adult; set => adult = value; }
+        public string Backdrop_path { get => backdrop_path; set => backdrop_path = value; }
+        public int[]? Genre_ids { get => genre_ids; set => genre_ids = value; }
+        public int Id { get => id; set => id = value; }
+        public string Original_language { get => original_language; set => original_language = value; }
+        public string Original_title { get => original_title; set => original_title = value; }
+        public string Overview { get => overview; set => overview = value; }
+        public float Popularity { get => popularity; set => popularity = value; }
+        public string Poster_path { get => poster_path; set => poster_path = value; }
+       
+        public string Title { get => title; set => title = value; }
+        public bool Video { get => video; set => video = value; }
+        public float Vote_average { get => vote_average; set => vote_average = value; }
+        public int Vote_count { get => vote_count; set => vote_count = value; }
+        public DateTime Release_date { get => release_date; set => release_date = value; }
+        public string Original_name { get => original_name; set => original_name = value; }
+        public String First_air_date { get => first_air_date; set => first_air_date = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
+ 
